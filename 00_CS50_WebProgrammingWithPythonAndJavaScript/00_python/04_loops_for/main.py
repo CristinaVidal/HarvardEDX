97a44480575337765a77562bc8f90b9d5d1e6076
CristinaVidal <vidal.cristina14@gmail.com>
2021-01-16 19:27:50 +0100

    Basic for loop examples

---
+++ 00_CS50_WebProgrammingWithPythonAndJavaScript/00_python/04_loops_for/main.py
@@ -0,0 +1,17 @@
+print("Basic array for:");
+for i in [1, 2, 3, 4, 5]:
+    print(i)
+
+print("\nBasic range for:");
+for i in range(6):
+    print(i)
+
+print("\nBasic string for:");
+name = "Cristina";
+for char in name:
+    print(char);
+
+print("\nBasic list for:");
+list = ["Cristina", "Lari", "Marta"];
+for item in list:
+    print(item);
