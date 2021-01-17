94dd93ecfb2e73c17b726cf6faf1728b53671920
CristinaVidal <vidal.cristina14@gmail.com>
2021-01-17 18:03:31 +0100

    Basic functions

---
+++ 00_CS50_WebProgrammingWithPythonAndJavaScript/00_python/05_functions/squares.py
@@ -0,0 +1,5 @@
+from functions import square
+#import functions --> Importa todo de functions.py. Habría que especificar functions.square
+
+for i in range(10):
+    print(f"The square of {i} is {square(i)}")
